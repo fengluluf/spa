c1ccd9c7c616961a5ca14829d82f928152b61c7a
fengluluf <2277196352@qq.com>
2018-03-13 21:18:42 +0800

    修改矩形计算

--- 01-first-app/01-first-app.js
+++ 01-first-app/01-first-app.js
@@ -4,8 +4,11 @@ window.onload = function(){
 	function click(){
 		var width = document.getElementById("rectangle__width").value;
 		var height = document.getElementById("rectangle__height").value;
-		document.getElementById("rectangle__per").value= width*2+height*2;
-		document.getElementById("rectangle__area").value = width*height;
+		document.getElementById("rectangle__per").value= parseFloat((width*2+height*2).toFixed(10));
+		document.getElementById("rectangle__area").value = parseFloat((width*height).toFixed(10));
 	}
 	document.getElementById("rectangle__calc").onclick=click;
+	function per(){
+
+	}
 }
