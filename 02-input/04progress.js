112995e796db6daa29ab01bbb58e0807cc49c620
fengluluf <2277196352@qq.com>
2018-03-13 15:59:54 +0800

    改进进度条控件

--- 02-input/04progress.js
+++ 02-input/04progress.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 	var $pro = $('#pro');
-	var timer;
+	var timer=0;
 	var value = 0;
 	$('#start').click(function(){
+		if(timer!==0)return;
 		timer = window.setInterval(function(){
 			$pro.attr('value',value++)
 		},50)
 	});
 	$('#end').click(function(){
 		clearInterval(timer);
+		timer = 0;
 	});
 	$('#reset').click(function(){
 		$pro.attr('value',value=0);
