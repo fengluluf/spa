b3269951a02c6c99650f530c021d0ea626236759
fengluluf <2277196352@qq.com>
2018-03-15 15:43:21 +0800

    修改

--- 03-validation/01-field.js
+++ 03-validation/01-field.js
@@ -3,34 +3,38 @@ $(document).ready(function(){
 	var $height = $("#rectangle__height");
 	var $widthp = $("#widthp");
 	var $heightp = $("#heightp");
+	var p=false;
 	//加载后获取焦点
 	$width.focus();
 	$("#rectangle__calc").click(function(){
+		if(p){return;}
 		var width = $width.val();
 		var height = $height.val();		
 		$("#rectangle__per").val(parseFloat((width*2+height*2).toFixed(10)));
 		$("#rectangle__area").val(parseFloat((width*height).toFixed(10)));
 	});
-
 	$width.blur(function(){
-		proving1($width);
+	    p=proving1($width);
 	});
 	$height.blur(function(){
-		proving2($height);
+		p=proving2($height);
 	});
 	function proving1(e){
 		var width = $width.val();
 		var data = $(e);
 		if(data.val()===''){
 			$widthp.html("宽度不能为空");
+			$width.focus();
 			return;
 		}
 		if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
       		$widthp.html( '宽度必须是数值');
+      		$width.focus();
       		return;
     	}
     	if(window.Number(data.val()) < 0) {
       		$widthp.html( '宽度必须大于零');
+      		$width.focus();
       		return;
     	}
 		$widthp.html('');
@@ -40,14 +44,17 @@ $(document).ready(function(){
 		var data = $(e);
 		if(data.val()===''){
 			$heightp.html("长度不能为空");
+			$height.focus();
 			return;
 		}
 		if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
       		$heightp.html( '长度必须是数值');
+      		$height.focus();
       		return;
     	}
     	if(window.Number(data.val()) < 0) {
       		$heightp.html( '长度必须大于零');
+      		$height.focus();
       		return;
     	}
 		$heightp.html('');
--- 03-validation/03-form.js
+++ 03-validation/03-form.js
@@ -7,45 +7,48 @@ $(document).ready(function(){
 	$width.focus();
 	$("#rectangle__calc").click(function(){
 		proving1($width);
+		function proving1(e){
+			var width = $width.val();
+			var data = $(e);
+			if(data.val()===''){
+				$widthp.html("宽度不能为空");
+				return;
+			}
+			if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
+	      		$widthp.html( '宽度必须是数值');
+	      		return;
+	    	}
+	    	if(window.Number(data.val()) < 0) {
+	      		$widthp.html( '宽度必须大于零');
+	      		return;
+	    	}
+			$widthp.html('');
+			var width = $width.val();
+			var height = $height.val();		
+			$("#rectangle__per").val(parseFloat((width*2+height*2).toFixed(10)));
+			$("#rectangle__area").val(parseFloat((width*height).toFixed(10)));
+		}
 		proving2($height);
-		var width = $width.val();
-		var height = $height.val();		
-		$("#rectangle__per").val(parseFloat((width*2+height*2).toFixed(10)));
-		$("#rectangle__area").val(parseFloat((width*height).toFixed(10)));
-
-	});
-	function proving1(e){
-		var width = $width.val();
-		var data = $(e);
-		if(data.val()===''){
-			$widthp.html("宽度不能为空");
-			return;
-		}
-		if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
-      		$widthp.html( '宽度必须是数值');
-      		return;
-    	}
-    	if(window.Number(data.val()) < 0) {
-      		$widthp.html( '宽度必须大于零');
-      		return;
-    	}
-		$widthp.html('');
-	}
-	function proving2(e){
-		var height = $height.val();
-		var data = $(e);
-		if(data.val()===''){
-			$heightp.html("长度不能为空");
-			return;
-		}
-		if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
-      		$heightp.html( '长度必须是数值');
-      		return;
-    	}
-    	if(window.Number(data.val()) < 0) {
-      		$heightp.html( '长度必须大于零');
-      		return;
-    	}
-		$heightp.html('');
-	}
+		function proving2(e){
+			var height = $height.val();
+			var data = $(e);
+			if(data.val()===''){
+				$heightp.html("长度不能为空");
+				return;
+			}
+			if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data.val())) {
+	      		$heightp.html( '长度必须是数值');
+	      		return;
+	    	}
+	    	if(window.Number(data.val()) < 0) {
+	      		$heightp.html( '长度必须大于零');
+	      		return;
+	    	}
+			$heightp.html('');
+			var width = $width.val();
+			var height = $height.val();		
+			$("#rectangle__per").val(parseFloat((width*2+height*2).toFixed(10)));
+			$("#rectangle__area").val(parseFloat((width*height).toFixed(10)));
+		}		
+	});	
 })
